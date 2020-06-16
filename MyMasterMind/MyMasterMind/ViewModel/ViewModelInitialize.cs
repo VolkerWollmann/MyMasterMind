80aceda672afa3c1bfca2f8f40f618b01b46697d
Volker Wollmann <64246526+VolkerWollmann@users.noreply.github.com>
2020-06-16 11:20:43 +0200

    InitializeViewModels

--- MyMasterMind/MyMasterMind/ViewModel/ViewModelInitialize.cs
+++ MyMasterMind/MyMasterMind/ViewModel/ViewModelInitialize.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
+using MyMasterMind.Controls;
 
 namespace MyMasterMind.ViewModel
 {
-	class ViewModelInitialize
-	{
+	public class ViewModelInitialize
+	{		
+		public static void InitializeViewModels(Grid grid)
+		{
+			var myMasterMindViewModel  = grid.Children.OfType<MasterMindBoard>().First();
+			var myMasterMindCommands = grid.Children.OfType<MasterMindCommands>().First();
+		}
 	}
 }
