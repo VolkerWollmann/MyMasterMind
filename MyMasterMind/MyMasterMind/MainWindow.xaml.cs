fde9f5846c921bc1e99b0adb52e91628c5b49a8d
Volker Wollmann <64246526+VolkerWollmann@users.noreply.github.com>
2020-06-16 09:13:05 +0200

    Creating ViewModel

--- MyMasterMind/MyMasterMind/MainWindow.xaml.cs
+++ MyMasterMind/MyMasterMind/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MyMasterMind.ViewModel;
 
 namespace MyMasterMind
 {
@@ -23,6 +24,7 @@ namespace MyMasterMind
 		public MainWindow()
 		{
 			InitializeComponent();
+			ViewModelInitialize.InitializeViewModels(MyMasterMindGrid);
 		}
 	}
 }
