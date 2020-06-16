a656c646e509799720932369398e26876ef3b55d
Volker Wollmann <64246526+VolkerWollmann@users.noreply.github.com>
2020-06-16 11:37:11 +0200

    ViewModel 2

--- MyMasterMind/MyMasterMind/Interfaces/IMasterMindBoardView.cs
+++ MyMasterMind/MyMasterMind/Interfaces/IMasterMindBoardView.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace MyMasterMind.Interfaces
 {
-	class IMasterMindBoardView
+	interface IMasterMindBoardView
 	{
 	}
 }
--- MyMasterMind/MyMasterMind/ViewModel/MyMasterMindViewModel.cs
+++ MyMasterMind/MyMasterMind/ViewModel/MyMasterMindViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MyMasterMind.Controls;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,15 @@ using System.Threading.Tasks;
 
 namespace MyMasterMind.ViewModel
 {
-	class MyMasterMindViewModel
+	public class MyMasterMindViewModel
 	{
+		MasterMindBoard MasterMindBoard;
+		MasterMindCommands MasterMindCommands;
+
+		public MyMasterMindViewModel(MasterMindBoard masterMindBoard, MasterMindCommands masterMindCommands)
+		{
+			MasterMindBoard = masterMindBoard;
+			MasterMindCommands = masterMindCommands;
+		}
 	}
 }
